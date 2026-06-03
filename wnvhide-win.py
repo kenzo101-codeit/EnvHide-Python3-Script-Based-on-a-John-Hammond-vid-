3ac25f5175ef2d1b7e9b3825912def6da54a55f9
kenzo101-codeit <kenzo101studios@gmail.com>
2026-06-03 12:43:37 -0300

    Criar wnvhide-win.py

---
+++ wnvhide-win.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+import os
+import string
+from pprint import pprint
+import random
+
+env_vars = [
+    "ALLUSERSPROFILE",
+    "APPDATA",
+    "CommonProgramFiles",
+    "CommonProgramW6432",
+    "ComSpec",
+    "PATHEXT",
+    "ProgramData",
+    "ProgramFiles",
+    "ProgramW6432",
+    "PSModulePath",
+    "PUBLIC",
+    "SystemDrive",
+    "SystemRoot",
+    "TEMP",
+    "TMP",
+    "windir"
+
+]
+
+# Build ENV Mapping Dictionary
+env_mapping = {}
+
+for character in string.printable:
+    env_mapping[character] = {}
+    for var in env_vars:
+        value = os.getenv(var)
+
+        if not value:
+            continue
+
+        if character in value:
+            env_mapping[character][var] = []
+            for i, c in enumerate(value):
+                if character == c:
+                   env_mapping[character][var].append(i)
+
+
+# FUNCTIONS DEFINITIONS
+
+def envhide_obfuscate(string):
+    obf_code = []
+
+    for c in string:
+        possible_vars = list(env_mapping[c].keys())
+
+        if not possible_vars:
+            obf_code.append(f'[char]{ord(c)}')
+            continue
+
+        chosen_var = random.choice(possible_vars)
+        possible_indices = env_mapping[chosen_var]
+        
+        # print(f"{chosen_var=} {possible_indices=}")
+        
+        chosen_index =  random.choice(possible_indices)
+        
+        new_character = os.getenv(chosen_var)[c][chosen_var][chosen_index]
+        
+        pwsh_syntax = f'$env:{chosen_var}[{chosen_index}]'
+        
+        obf_code.append(pwsh_syntax)
+
+    return obf_code
+
+def powershell_obfuscate(string):
+    iex = envhide_obfuscate('iex')
+    pieces = envhide_obfuscate(string)
+    
+    iex_stage = f'& {"".join(iex)} -Join ${random.randint(1,99999)}'
+    
+    payload_stage = f'& {"".join(pieces)} -Join ${random.randint(1,99999)}'
+    
+    # old return f'& {iex} {payload} -Join ${random.randint(1,99999)}'
+    return f'& {iex_stage} {payload_stage}'
+
+pwsh_command = 'Write-Output 420'
+
+pprint(envhide_obfuscate(pwsh_command))
