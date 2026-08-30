316b9543325567e025b05d45af1a85f56b329641
kenzo101-codeit <kenzo101studios@gmail.com>
2026-08-30 15:59:05 -0300

    Fix indexing and print statement in envhide-linux.py

--- envhide-linux.py
+++ envhide-linux.py
@@ -2,7 +2,7 @@
 
 import os
 import string
-from pprint import pprint
+# from pprint import pprint
 import random
 
 env_vars = [
@@ -53,13 +53,13 @@ def envhide_obfuscate(string):
             continue
 
         chosen_var = random.choice(possible_vars)
-        possible_indices = env_mapping[chosen_var]
+        possible_indices = env_mapping[c][chosen_var]
         
         # print(f"{chosen_var=} {possible_indices=}")
         
         chosen_index =  random.choice(possible_indices)
         
-        new_character = os.getenv(chosen_var)[c][chosen_var][chosen_index]
+        new_character = os.getenv(chosen_var)[chosen_index]
         
         pwsh_syntax = f'$env:{chosen_var}[{chosen_index}]'
         
@@ -80,4 +80,4 @@ def powershell_obfuscate(string):
 
 pwsh_command = 'Write-Output 420'
 
-pprint(powershell_obfuscate(pwsh_command))
+print(powershell_obfuscate(pwsh_command))
